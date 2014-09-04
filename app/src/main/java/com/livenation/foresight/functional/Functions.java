c66edf8806c81a7fd94c9ed1e7805a3fa89e3f26
Kevin MacWhinnie <kevin.macwhinnie@me.com>
2014-09-04 14:13:21 -0700

    Use the function types

--- app/src/main/java/com/livenation/foresight/functional/Functions.java
+++ app/src/main/java/com/livenation/foresight/functional/Functions.java
@@ -3,17 +3,18 @@ package com.livenation.foresight.functional;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.livenation.foresight.functional.types.Function;
+import com.livenation.foresight.functional.types.Predicate;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
-import rx.functions.Func1;
-
 public final class Functions {
-    public static <I, O> ArrayList<O> mapList(@Nullable Collection<I> collection, @NonNull Func1<I, O> mapper) {
+    public static <I, O> ArrayList<O> mapList(@Nullable Collection<I> collection, @NonNull Function<I, O> mapper) {
         ArrayList<O> accumulator = new ArrayList<>();
         if (collection != null) {
             for (I value : collection) {
-                O result = mapper.call(value);
+                O result = mapper.apply(value);
                 if (result == null)
                     continue;
 
@@ -23,11 +24,11 @@ public final class Functions {
         return accumulator;
     }
 
-    public static <T> ArrayList<T> filterList(@Nullable Collection<T> collection, @NonNull Func1<T, Boolean> predicate) {
+    public static <T> ArrayList<T> filterList(@Nullable Collection<T> collection, @NonNull Predicate<T> predicate) {
         ArrayList<T> accumulator = new ArrayList<>();
         if (collection != null) {
             for (T value : collection) {
-                if (predicate.call(value))
+                if (predicate.test(value))
                     accumulator.add(value);
             }
         }
